1f17e0d53a553f507e96edb3b496acedba80f990
John Puddifoot <john@eastpoint.co.uk>
2019-06-08 08:25:36 +0100

    Structure and Rendering for complex, continuous lines

---
+++ Mapsui.Rendering.Skia/ContinuousLineStringRenderer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
+using BruTile.Tms;
+using Mapsui.Geometries;
+using Mapsui.Providers;
+using Mapsui.Styles;
+using SkiaSharp;
+
+namespace Mapsui.Rendering.Skia
+{
+    public static class ContinuousLineStringRenderer
+    {
+        public static TimeSpan TotalTimeSpan = TimeSpan.Zero;
+
+        public static void Draw(SKCanvas canvas, IReadOnlyViewport viewport, IStyle style, IFeature feature,
+            IGeometry geometry,
+            float opacity, float labelTextPadding)
+        {
+            var continuousLineString = (ContinuousLineString) geometry;
+            if (continuousLineString.Vertices.Count <= 1) return;
+
+            
+            float lineWidth = 1;
+            var lineColor = new Color();
+
+            var vectorStyle = style as VectorStyle;
+            var strokeCap = PenStrokeCap.Butt;
+            var strokeJoin = StrokeJoin.Miter;
+            var strokeMiterLimit = 4f;
+            var strokeStyle = PenStyle.Solid;
+            float[] dashArray = null;
+
+            if (vectorStyle != null)
+            {
+                lineWidth = (float)vectorStyle.Line.Width;
+                lineColor = vectorStyle.Line.Color;
+                strokeCap = vectorStyle.Line.PenStrokeCap;
+                strokeJoin = vectorStyle.Line.StrokeJoin;
+                strokeMiterLimit = vectorStyle.Line.StrokeMiterLimit;
+                strokeStyle = vectorStyle.Line.PenStyle;
+                dashArray = vectorStyle.Line.DashArray;
+            }
+
+
+            using (var paint = new SKPaint { IsAntialias = true })
+            {
+                paint.IsStroke = true;
+                paint.StrokeWidth = lineWidth;
+                paint.Color = lineColor.ToSkia(opacity);
+                paint.StrokeCap = strokeCap.ToSkia();
+                paint.StrokeJoin = strokeJoin.ToSkia();
+                paint.StrokeMiter = strokeMiterLimit;
+                if (strokeStyle != PenStyle.Solid)
+                    paint.PathEffect = strokeStyle.ToSkia(lineWidth, dashArray);
+                else
+                    paint.PathEffect = null;
+
+                DateTime startTime = DateTime.UtcNow;
+
+                // get the line start points, but reduce sample set up 500x
+                var points = continuousLineString
+                    .Vertices
+                    .Where(x => x.BoundingBox.Intersects(viewport.Extent))
+                    .ToArray();
+
+                var resampleTimeTaken = DateTime.UtcNow - startTime;
+                TotalTimeSpan = TotalTimeSpan.Add(resampleTimeTaken);
+                Console.WriteLine($"********* MultiLineStringRenderer.Draw - Prep data: *** took {resampleTimeTaken.TotalSeconds}.{resampleTimeTaken.Milliseconds}.");
+                
+                var path = points.ToContinuousSkiaPath(viewport, canvas.LocalClipBounds, 300);
+                canvas.DrawPath(path, paint);
+
+                var timeTaken = DateTime.UtcNow - startTime;
+                TotalTimeSpan = TotalTimeSpan.Add(timeTaken);
+                Console.WriteLine($"********* MultiLineStringRenderer.Draw ({points?.Count()} items) *** took {timeTaken.TotalSeconds}.{timeTaken.Milliseconds}. Total time used:  {TotalTimeSpan.TotalSeconds}.{TotalTimeSpan.Milliseconds}");
+            }
+        }
+    }
+}
